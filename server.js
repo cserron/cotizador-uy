92798b691ec446f6ef9f5d1f632c3f431d9a5f9b
Chris Serron <christian@ivorydigitalnyc.com>
2016-10-08 03:07:17 -0300

    Details in configuration for port and heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app     = express();
-
+let pjson  = require('./package.json');
 // Banks ===============================
 let brou    = require('./controllers/brou.js') 
 
 
 
 app.use('/brou', brou);
-app.listen( process.env.npm_package_config_port )
+app.listen(pjson.config.port);
 console.log('"One man’s crappy software is another man’s full time job." -Jessica Gaston');
-console.log("Running on port: " + process.env.npm_package_config_port)
+console.log("Running on port: " + pjson.config.port)
 
 exports = module.exports = app;
