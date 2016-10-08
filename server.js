2af2e731b824556762021d840dacf4cf2d3c766c
Chris Serron <christian@ivorydigitalnyc.com>
2016-10-08 02:35:32 -0300

    Port and start script included in package.json

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ let brou    = require('./controllers/brou.js')
 
 
 app.use('/brou', brou);
-app.listen('8081')
+app.listen( process.env.npm_package_config_port )
 console.log('"One man’s crappy software is another man’s full time job." -Jessica Gaston');
-console.log('Server started...');
+console.log("Running on port: " + process.env.npm_package_config_port)
+
 exports = module.exports = app;
